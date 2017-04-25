70c23399789e3d1a058f927fd0e7021f7e5f4f0c
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-04-25 08:42:43 +0200

    Clase principal

---
+++ Boletin 24_1/Boletin24_1/src/boletin24_1/Boletin24_1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin24_1;
+
+/**
+ *
+ * @author Adry
+ */
+public class Boletin24_1 {
+
+    
+    public static void main(String[] args) {
+       Ventana obx1=new Ventana();
+       obx1.setVisible(true);
+    }
+    
+}
